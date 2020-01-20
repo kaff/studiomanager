b97fc2e58a3918d79a7f501d882f2fd83e3722dc
Tomasz Paloc <tomasz.paloc@gmail.com>
2020-01-20 07:24:25 +0000

    [bata] - build process

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd build
+git clone https://github.com/kaff/studiomanager.git b0
+cd b0
+docker-compose up -d
+docker-compose exec php bash -c "composer install --no-dev"
+docker-compose down
+docker build -f dockerfile-ci -t studio-manager-ci:b1 .
